5949da967e13e22e33624febf004f608cc8274be
ritik gupta <ritikguptacs47@gmail.com>
2023-01-08 21:11:20 +0530

    q6

---
+++ Medium Problems/ZigZagTrav.java
@@ -0,0 +1,28 @@
+import java.util.*;
+class Solution {
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+        Queue <TreeNode> queue = new LinkedList <TreeNode>();
+        List<List<Integer>> ans = new ArrayList<>();
+
+        if (root == null) return ans;
+
+        queue.offer(root);
+        boolean flag = true;
+
+        while (!queue.isEmpty()) {
+            int len = queue.size();
+            ArrayList <Integer> curr = new ArrayList <Integer>();
+            for (int i = 0; i < len; i++) {
+                if (queue.peek().left != null) queue.offer(queue.peek().left);
+
+                if (queue.peek().right != null) queue.offer(queue.peek().right);
+
+                if (flag == true) curr.add(queue.poll().val);
+                else curr.add(0, queue.poll().val);
+            }
+            flag = !flag;
+            ans.add(curr);
+        }
+        return ans;
+    }
+}
